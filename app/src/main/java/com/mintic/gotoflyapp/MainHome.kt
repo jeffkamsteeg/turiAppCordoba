dc47ea5fc9146459b70da378f874be2ca5c93cdb
KatherinePlata <katherineplata.tic@gmail.com>
2021-11-20 09:17:34 -0500

    Actualizacion de layoutConstraint

---
+++ app/src/main/java/com/mintic/gotoflyapp/MainHome.kt
@@ -0,0 +1,11 @@
+package com.mintic.gotoflyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_home)
+    }
+}
